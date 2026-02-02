1157f82550f5282a26f2db8b72af7bba5f4278a5
ananthan97 <harikumar.a@northeastern.edu>
2026-02-03 04:21:33 +0530

    Add in-memory token store implementation

--- oauth-integration-service/src/main/java/com/ois/oauthintegrationservice/infra/token/InMemoryTokenStore.java
+++ oauth-integration-service/src/main/java/com/ois/oauthintegrationservice/infra/token/InMemoryTokenStore.java
@@ -1,4 +1,30 @@
 package com.ois.oauthintegrationservice.infra.token;
 
-public class InMemoryTokenStore {
+import com.ois.oauthintegrationservice.core.token.Token;
+import com.ois.oauthintegrationservice.core.token.TokenStore;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+@Component
+public class InMemoryTokenStore implements TokenStore {
+
+    private final ConcurrentMap<String, Token> store = new ConcurrentHashMap<>();
+
+    @Override
+    public void save(String providerId, Token token){
+        store.put(providerId, token);
+    }
+
+    @Override
+    public Optional<Token> get(String providerId) {
+        return Optional.ofNullable(store.get(providerId));
+    }
+
+    @Override
+    public void remove(String providerId){
+        store.remove(providerId);
+    }
 }
